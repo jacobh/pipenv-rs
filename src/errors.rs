f32cfa4f90956b4a73f1ccbadf9b2d6610c91e6f
Jacob Haslehurst <jacob@haslehurst.net>
2017-06-14 21:43:17 +1000

    replace manually typed errors with error_chain

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,19 @@
-error_chain!{}
+error_chain!{
+    foreign_links {
+        SerdeJson(::serde_json::Error);
+        Io(::std::io::Error);
+        SemVerReqParse(::semver::ReqParseError);
+        Zip(::zip::result::ZipError);
+    }
+    errors {
+            PackageNameRegexFailed(s: String) {
+                description("Package name regex failed")
+                display("Package name regex failed: `{}`", s)
+            }
+            ArchiveFileNotFound(s: String) {
+                description("File not found in archive")
+                display("File not found in archive: `{}`", s)
+            }
+    }
+}
+
--- src/parse_release.rs
+++ src/parse_release.rs
@@ -1,21 +1,17 @@
 use regex::Regex;
-use std::error;
-use std::fmt;
 use std::io;
 use std::io::Read;
 use flate2::read::GzDecoder;
 use tar::Archive as TarArchive;
 use zip::read::ZipArchive;
-use zip::result::ZipError;
 use semver;
 use serde_json;
 
 use version_req::PackageVersionReq;
 use release::{ReleaseType, WheelMetadata};
+use errors::*;
 
-pub type ReleaseParseResult<T> = Result<T, ReleaseParseError>;
-
-fn get_wheel_metadata_from_archive_file<R>(mut file: R) -> ReleaseParseResult<WheelMetadata>
+fn get_wheel_metadata_from_archive_file<R>(mut file: R) -> Result<WheelMetadata>
     where R: io::Read
 {
     let mut archive = {
@@ -31,27 +27,27 @@ fn get_wheel_metadata_from_archive_file<R>(mut file: R) -> ReleaseParseResult<Wh
             return Ok(wheel_meta);
         }
     }
-    Err(ReleaseParseError::FileNotFound(".dist-info/metadata.json".to_owned()))
+    bail!(ErrorKind::ArchiveFileNotFound(".dist-info/metadata.json".to_owned()));
 }
 
-fn parse_requires_txt_line(line: &str) -> ReleaseParseResult<PackageVersionReq> {
+fn parse_requires_txt_line(line: &str) -> Result<PackageVersionReq> {
     lazy_static! {
         static ref PACKAGE_NAME_RE: Regex = Regex::new(r"\w+").unwrap();
         static ref VERSION_REQ_RE: Regex = Regex::new(r"[<=>]{1,2}\d+(\.\d+){0,2}").unwrap();
     }
     let package_name = PACKAGE_NAME_RE
         .find(line)
-        .ok_or_else(|| ReleaseParseError::PackageNameRegexFailed(line.to_owned()))?
+        .ok_or_else(|| ErrorKind::PackageNameRegexFailed(line.to_owned()))?
         .as_str()
         .to_owned();
-    let version_reqs: Result<Vec<semver::VersionReq>, semver::ReqParseError> = VERSION_REQ_RE
+    let version_reqs: Result<Vec<semver::VersionReq>> = VERSION_REQ_RE
         .find_iter(line)
-        .map(|x| semver::VersionReq::parse(x.as_str()))
+        .map(|x| semver::VersionReq::parse(x.as_str()).map_err(|e| e.into()))
         .collect();
     Ok(PackageVersionReq::new(package_name, version_reqs?))
 }
 
-fn parse_requires_txt(text: &str) -> ReleaseParseResult<Vec<PackageVersionReq>> {
+fn parse_requires_txt(text: &str) -> Result<Vec<PackageVersionReq>> {
     text.split("\n")
         .filter(|line| line != &"")
         .take_while(|line| !line.starts_with("["))
@@ -61,7 +57,7 @@ fn parse_requires_txt(text: &str) -> ReleaseParseResult<Vec<PackageVersionReq>> 
 
 pub fn parse_release_requirements<R>(file: R,
                                      release_type: ReleaseType)
-                                     -> ReleaseParseResult<Vec<PackageVersionReq>>
+                                     -> Result<Vec<PackageVersionReq>>
     where R: io::Read
 {
     match release_type {
@@ -85,76 +81,9 @@ pub fn parse_release_requirements<R>(file: R,
                     return Ok(parse_requires_txt(&requires_txt)?);
                 }
             }
-            Err(ReleaseParseError::FileNotFound(".egg-info/requires.txt".to_owned()))
+            bail!(ErrorKind::ArchiveFileNotFound(".egg-info/requires.txt".to_owned()));
         }
         _ => unimplemented!(),
-    }
-}
-
-#[derive(Debug)]
-pub enum ReleaseParseError {
-    Io(io::Error),
-    Zip(ZipError),
-    ReqParse(semver::ReqParseError),
-    Serde(serde_json::Error),
-    PackageNameRegexFailed(String),
-    FileNotFound(String),
-}
-impl fmt::Display for ReleaseParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            ReleaseParseError::Io(ref err) => err.fmt(f),
-            ReleaseParseError::Zip(ref err) => err.fmt(f),
-            ReleaseParseError::ReqParse(ref err) => err.fmt(f),
-            ReleaseParseError::Serde(ref err) => err.fmt(f),
-            ReleaseParseError::PackageNameRegexFailed(ref s) => {
-                write!(f, "Regex against package name `{}` failed", s.as_str())
-            }
-            ReleaseParseError::FileNotFound(ref s) => {
-                write!(f, "Could not find file: `{}`", s.as_str())
-            }
-        }
-    }
-}
-impl error::Error for ReleaseParseError {
-    fn description(&self) -> &str {
-        match *self {
-            ReleaseParseError::Io(ref err) => err.description(),
-            ReleaseParseError::Zip(ref err) => err.description(),
-            ReleaseParseError::ReqParse(ref err) => err.description(),
-            ReleaseParseError::Serde(ref err) => err.description(),
-            ReleaseParseError::PackageNameRegexFailed(..) => "package name regex failed",
-            ReleaseParseError::FileNotFound(..) => "file not found",
-        }
-    }
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            ReleaseParseError::Io(ref err) => Some(err),
-            ReleaseParseError::Zip(ref err) => Some(err),
-            ReleaseParseError::ReqParse(ref err) => Some(err),
-            ReleaseParseError::Serde(ref err) => Some(err),
-            _ => None,
-        }
-    }
-}
-impl From<io::Error> for ReleaseParseError {
-    fn from(err: io::Error) -> ReleaseParseError {
-        ReleaseParseError::Io(err)
-    }
-}
-impl From<ZipError> for ReleaseParseError {
-    fn from(err: ZipError) -> ReleaseParseError {
-        ReleaseParseError::Zip(err)
-    }
-}
-impl From<semver::ReqParseError> for ReleaseParseError {
-    fn from(err: semver::ReqParseError) -> ReleaseParseError {
-        ReleaseParseError::ReqParse(err)
-    }
-}
-impl From<serde_json::Error> for ReleaseParseError {
-    fn from(err: serde_json::Error) -> ReleaseParseError {
-        ReleaseParseError::Serde(err)
     }
 }
 
